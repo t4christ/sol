355517c6e2ceb1f4d782cd3876f20499837eca43
tdrohealth <tbakare@drohealth.com>
2022-01-24 13:02:37 +0100

    update html file

--- js/main.js
+++ js/main.js
@@ -54,5 +54,15 @@ const modalClose = document.querySelector('.close')
 })
 
 
+// const cards = document.querySelectorAll('.card')
 
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle('show', entry.isIntersecting)
+//         if(entry.isIntersecting) observer.unobserve(entry.target)
+//     })
+// })
 
+// cards.forEach(card =>{
+//     observer.observe(card)
+// })
